f4dff27c06f46b82bb10d418154fd61b6ce2927a
Wilson Ferreira Junior <wilson.f.jr@gmail.com>
2023-07-28 16:38:03 -0300

    Add output in Resource_Group

--- azure/azurerm_management_lock/main.tf
+++ azure/azurerm_management_lock/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_management_lock" "lock" {
   name       = var.name
-  scope      = var.scope
+  scope      = join(", ", var.scope)
   lock_level = var.lock_level
   notes      = var.note
 }
