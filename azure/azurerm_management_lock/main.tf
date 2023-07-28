efd1e9998e1ef8b2d32fcba60880679f605d45a6
Wilson Ferreira Junior <wilson.f.jr@gmail.com>
2023-07-28 16:59:43 -0300

    Add output in Resource_Group

--- azure/azurerm_management_lock/main.tf
+++ azure/azurerm_management_lock/main.tf
@@ -1,13 +1,12 @@
+# modules/azurerm_management_lock/main.tf
+
 locals {
   resource_group_scopes = [for id in var.scope : "/subscriptions/35a89c93-cf4c-47cf-a4b0-c1db8f4241d2/resourceGroups/${id}"]
 }
 
 resource "azurerm_management_lock" "lock" {
   name       = var.name
-
-  // Use a variável local "resource_group_scopes" em vez de "var.scope"
-  scope      = local.resource_group_scopes
-
+  scope      = local.resource_group_scopes  # Usando a variável local como valor para "scope"
   lock_level = var.lock_level
   notes      = var.note
 }
