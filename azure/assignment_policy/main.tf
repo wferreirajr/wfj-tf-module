62e501f1d1dbd93330d6adb13a9b1598a5419306
Wilson Ferreira Junior <wilson.f.jr@gmail.com>
2023-08-04 23:44:27 -0300

    .

--- azure/assignment_policy/main.tf
+++ azure/assignment_policy/main.tf
@@ -1,5 +1,8 @@
 # main.tf
 resource "azurerm_subscription_policy_assignment" "assignment_policy" {
+
+  count = var.apply_assignment_policy ? 1 : 0
+
   name                 = var.assignment_policy_name
   display_name         = var.assignment_policy_display_name
   description          = "[manager-by: IaC] - ${var.assignment_policy_description}"
--- azure/assignment_policy/variables.tf
+++ azure/assignment_policy/variables.tf
@@ -1,5 +1,11 @@
 # variables.tf
 
+variable "apply_assignment_policy" {
+  description = "Flag to create the Azure Assignment Policy"
+  type        = bool
+  default     = false
+}
+
 variable "assignment_policy_name" {
   type        = string
   description = "Name for policy"
