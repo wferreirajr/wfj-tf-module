e07b96a9c04c20afafc0caae999f071bc140458a
Wilson Ferreira Junior <wilson.f.jr@gmail.com>
2023-08-31 21:25:34 -0300

    .

--- azure/virtual_network/main.tf
+++ azure/virtual_network/main.tf
@@ -5,14 +5,14 @@ resource "azurerm_virtual_network" "wfj-vnet" {
   address_space       = [ var.vnet_cidr ]
   location            = var.location
   resource_group_name = var.resource_group_name
-  
-  dns_servers = var.dns_servers == "" ? null : var.dns_servers
+
+  dns_servers = var.dns_servers
 
   dynamic "subnet" {
     for_each = var.subnets
     content {
-      name           = subnet.value.name == "" ? null : subnet.value.name
-      address_prefix = subnet.value.address_prefix == "" ? null : subnet.value.address_prefix
+      name           = subnet.value.name
+      address_prefix = subnet.value.address_prefix
     }
   }
 
